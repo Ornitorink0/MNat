20ea6f8cb7f90c45b79005299665f922c8633ebc
Ornitorink0 <matteogurri1@gmail.com>
2025-06-24 16:12:13 +0200

    [fix] correctly created running script

--- run.sh
+++ run.sh
@@ -1,1 +1,17 @@
-source ./bin/activate
+#!/bin/bash
+
+if [ "$1" = "clean" ]; then
+    echo "Cleaning virtual environment..."
+    rm -rf ./venv
+    echo "Cleaned virtual environment."
+else
+    echo "Creating virtual environment..."
+    python3 -m venv ./venv
+    echo "Activating virtual environment..."
+    source ./venv/bin/activate
+    echo "Installing dependencies..."
+    ./venv/bin/pip install -r requirements.txt
+    echo "Starting application..."
+    sudo ./venv/bin/python src/app.py
+fi
+
